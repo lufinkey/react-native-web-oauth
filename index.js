b58fcce11b44e21912776504a2093baea0a0601e
lufinkey <luisfinke@gmail.com>
2018-01-10 15:39:02 -0500

    add OAuthActivity to manifest and fix import

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 import { NativeModules } from 'react-native';
 
-console.log("Native modules");
-console.dir(NativeModules);
-export default WebOAuth = NativeModules.RNWebOAuth;
+module.exports = NativeModules.RNWebOAuth;
