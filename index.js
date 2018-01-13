6882f770b981d8e00f043cab615c2fb587e02d74
lufinkey <luisfinke@gmail.com>
2018-01-12 23:55:22 -0500

    Copy options and build query string in login function

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 
 import { NativeModules } from 'react-native';
+import QueryString from 'querystring';
 
 const WebOAuth = NativeModules.RNWebOAuth;
 
 WebOAuth.login = function(options, callback)
 {
-	WebOAuth.performWebAuth(options, (response) => {
+	var authOptions = Object.assign({}, options);
+	
+	const params = authOptions.params;
+	const tokenSwapURL = authOptions.tokenSwapURL;
+	const tokenRefreshURL = authOptions.tokenRefreshURL;
+
+	delete authOptions.params;
+	delete authOptions.tokenSwapURL;
+	delete authOptions.tokenRefreshURL;
+
+	if(typeof params == 'object' && Object.keys(params).length > 0)
+	{
+		authOptions.url = authOptions.url+'?'+QueryString.stringify(params);
+	}
+
+	WebOAuth.performWebAuth(authOptions, (response) => {
+		//TODO perform token swap
 		if(callback)
 		{
-			callback(response);
+			callback(false, null);
 		}
 	});
 }
